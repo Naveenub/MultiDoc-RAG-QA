f775eb911af5a3b3965b3c533036a9fa62dca027
Naveen Badiger <83811961+Naveenub@users.noreply.github.com>
2026-02-06 20:40:30 +0530

    Create embedding_utils.py

---
+++ backend/utils/embedding_utils.py
@@ -0,0 +1,11 @@
+def create_embeddings(chunks):
+    """
+    Create embeddings (stub)
+    """
+    return chunks
+
+def insert_to_vector_db(embeddings):
+    """
+    Insert embeddings into FAISS / Pinecone
+    """
+    pass
